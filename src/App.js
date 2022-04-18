ee73b98c9ac72425932fdabb8cc09787192896ff
“Deyvin <deyvinvelasquez0202@gmail.com>
2022-04-18 15:51:00 -0400

    added some more styling on about section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import './App.css';
 import './NavBar.css'
+import './About.css'
 import Logo from './Picture.JPG'
-import Laptop from './laptop.jpeg'
+import Logo2 from './computerImg.png'
+import Logo3 from './soccerImg.jpeg'
+
 
 function App() {
   return (
@@ -13,13 +16,29 @@ function App() {
   <a href='#contact'>Contact</a>
   </div>
 
- <section>
-      <div>
-        <h1 className='header'>Deyvin Cano Velasquez</h1>
-        <img className='hero' src={Laptop}  alt='White computer'/>
-      </div>
-        <img className='picture' src={Logo} alt='Me sitting on a chair' /> 
-  </section>
+ <div>
+      <section className='header'>
+        <div className='picture1-con'>
+        <img  className='picture1' src={Logo2} alt='computer with screw driver coming out of screen'/>
+        </div>
+        <div className='picture2-con'>
+        <img className='picture2' src={Logo3} alt='soccer ball with an outline'/>
+        </div>
+        <h1 className='h1'>Deyvin Cano Velasquez</h1>
+        <h2 className='h2'> Software Engineer</h2>
+      </section>
+      <section className='about'>
+        <h2 className='about-header'> About </h2> 
+        <div className='image-container'>
+          <div className='hex'>
+            <div className='img'>
+              <img src={Logo} alt='Me sitting on a chair' /> 
+            </div>
+          </div>
+        </div>
+      </section>
+
+  </div>
     </>
   );
 }
